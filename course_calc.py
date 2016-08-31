5bfbfaca7ef2642733ad9ef2d154119e6daa66e6
Trevor Steen <trevor.steen@rackspace.com>
2016-08-31 14:21:11 -0500

    use regex to get lat/lon

--- course_calc.py
+++ course_calc.py
@@ -2,16 +2,27 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import requests
+import re
 
-#Get origin from user
-origin = raw_input("What is your origin? ")
+def get_lat_long(origin):
+	#Get origin from user
+	#origin = raw_input("What is your origin? ")
 
-#Got to FAA page to gather airport data
-soup = BeautifulSoup(requests.get("https://nfdc.faa.gov/nfdcApps/services/airportLookup/airportDisplay.jsp?airportId={0}".format(origin)).text)
+	#Got to FAA page to gather airport data
+	#soup = BeautifulSoup(requests.get("https://nfdc.faa.gov/nfdcApps/services/airportLookup/airportDisplay.jsp?airportId={0}".format(origin)).text)
+	#test
+	soup = BeautifulSoup(requests.get("https://nfdc.faa.gov/nfdcApps/services/airportLookup/airportDisplay.jsp?airportId=KCVB").text)
 
-tableSummary = soup.find('table', {'class' : 'table table-condensed table-borderless' })
+	tableSummary = soup.find('table', {'class' : 'table table-condensed table-borderless' })
 
-for row in tableSummary:
-	if "Latitude" in str(row):
-		elements = row.findAll("td")
-		print elements[1].text
+	lat_regex = '([0-9]{1,2}-[0-9]{1,2}-[0-9]{1,2}.[0-9]) [N,S]'
+	lon_regex = '([0-9]{1,2}-[0-9]{1,2}-[0-9]{1,2}.[0-9]) [E,W]'
+
+	for row in tableSummary:
+		if "Latitude" in str(row):
+			elements = str(row.findAll("td")[1].text)
+			lat = re.search(lat_regex, elements)
+			lon = re.search(lon_regex, elements)
+
+return lat, lon
+
